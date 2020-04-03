9116b348689d625e4b60fda3c3e53072cc58b6c4
Kosuke <gekkouzann334@gmail.com>
2020-04-04 01:34:09 +0900

    vue file commit

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,66 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<h1>Vue On Rails TODO List</h1>
+
+<div id="app">
+  <p>
+  <input type="text"
+    placeholder="TODOを入力"
+    v-model="newItemTitle"
+    v-on:keyup.enter="addTodo(newItemTitle)">
+  </p>
+
+  <ul>
+    <li v-for="item in items" :key="item.title">
+      <label v-bind:class="{ done: item.isChecked }">
+        <input type="checkbox" v-model="item.isChecked"> {{ item.title }}
+      </label>
+    </li>
+  </ul>
+</div>
+
+</body>
+</html>
 </template>
 
 <script>
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      items: [
+          { title: 'test1', isChecked: true },
+          { title: 'test2', isChecked: true },
+          { title: 'test3', isChecked: false },
+          { title: 'test4', isChecked: false },
+      ],
+      newItemTitle: ''
     }
-  }
+  },
+
+  methods: {
+    addTodo: function(newTitle){
+      this.items.push({
+        title: newTitle,
+        isChecked: false
+      });
+      this.newItemTitle = ''; //追加
+    },
+  },
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 p {
   font-size: 2em;
   text-align: center;
 }
+
+.done { text-decoration: line-through; }
 </style>
